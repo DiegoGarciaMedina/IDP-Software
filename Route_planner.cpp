13e02a8928b791a6684359b5e2013e6fd8432ab2
Diego García Medina <dg501@cam.ac.uk>
2017-11-10 18:06:02 +0000

    Update of minor changes in files and added the XML file to draw the flowchart in www.draw.io

--- Route_planner.cpp
+++ Route_planner.cpp
@@ -15,15 +15,13 @@ stopwatch watch;
 #define time_separation 500
 
 int main(){
-	
+
 #ifdef __arm__
-   if (!rlink.initialise ("127.0.0.1")) {          // setup for local hardware
+   if (!rlink.initialise ("127.0.0.1")) {         // setup for local hardware
 #else
-}
-   if (!rlink.initialise (ROBOT_NUM)) { 
+   if (!rlink.initialise (ROBOT_NUM)) {
 	   // setup the link
 #endif
-}
 
   val = rlink.request (TEST_INSTRUCTION); // send test instruction
   if (val == TEST_INSTRUCTION_RESULT) {   // check result
@@ -44,19 +42,19 @@ void delivery(string BoxType, string InitialPoint){
   case "O/C": D1(InitialPoint);
     cout << "O/C detected, heading to D1."
   case "Short": D1(InitialPoint);
-    cout << "O/CShort circuit detected, heading to D1."
+    cout << "Short circuit detected, heading to D1."
   case "1": D2(InitialPoint);
     cout << "Box type 1 detected, heading to D2."
   case "2": D4(InitialPoint);
     cout << "Box type 2 detected, heading to D4."
   case "3": D3(InitialPoint);
     cout << "Box type 3 detected, heading to D3."
-  default : cout << "The box has not been properly classified!" << endl; 
+  default : cout << "The box has not been properly classified!" << endl;
   }
 }
 
 void D1(string InitialPoint){
-  
+
   bool misleading_turn_table = false;
   bool reached_delivery_point = false;
 
@@ -66,7 +64,7 @@ void D1(string InitialPoint){
   rlink.command(MOTOR_3_GO,127);
   rlink.command(MOTOR_1_GO,127);
   while (watch.read() < time_separation){}
-    
+
   // turn 180
   watch.start();
   rlink.command(MOTOR_3_GO,127);
@@ -77,21 +75,21 @@ void D1(string InitialPoint){
   while ((misleading_turn_table == false) && (reached_delivery_point == false)){
     misleading_turn_table, reached_delivery_point = line_follower(InitialPoint,"D1","turn_table_left"); // **YET TO BE DONE**
     }
-    
+
   // decide whether it has reached the turn table or the delivery point
   if (misleading_turn_table == true){
-    // turn 90 anticlockwis
+    // turn 90 anticlockwise
     watch.start();
     rlink.command(MOTOR_3_GO,127);
     rlink.command(MOTOR_1_GO,127);
     while (watch.read() < time90turntable){}
-      
-    // reactivate line following after the 
+
+    // reactivate line following after the
     while (reached_delivery_point == false){
       misleading_turn_table, reached_delivery_point = line_follower("Lcross","D1","None");
     }
   }
-      
+
   delivery_mechanism(); // **YET TO BE DONE**
 
   }
