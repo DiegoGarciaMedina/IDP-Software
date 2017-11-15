93e8355aa2265d28f04c9d91930bc673ee02a853
D. Garcia Medina <dg501@ew127.eng.cam.ac.uk>
2017-11-15 12:16:39 +0000

    gotten rid of robot_link issue

--- movement.h
+++ movement.h
@@ -5,16 +5,15 @@
 #include <robot_link.h>
 using namespace std;
 #define ROBOT_NUM 14
-
-robot_link rlink;
+int speed, time_reverse;
 
 int read_sensors_line_following();
 int distance_sensor(int sensors);
 int line_following(int sensors);
-void turn_left(speed);
+int turn_left(speed);
 void turn_right(void);
 void turn_around(void);
-void reverse_robot(time_reverse);
+int reverse_robot(time_reverse);
 void straight_junction(void);
 void stop(void);
 void pick_up(void);
