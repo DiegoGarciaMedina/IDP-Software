3c27939de3d1fe7b7e26dde96b74b451f1316028
D. Garcia Medina <dg501@ew127.eng.cam.ac.uk>
2017-11-20 11:11:05 +0000

    creating the test file

---
+++ tests/bda.cpp
@@ -0,0 +1,7 @@
+//Basic demonstrability tests
+
+//test 1 Start robot pointing towards the ramp, turn left, at junction turn right, keep straight over other junction and stop when it detects a wall
+
+//test 2 Starting from 20cm away from the ramp (pointing towards it), move up the ramp and keep straight over junction and stop at delivery point
+
+//test 3 Start 20 cm from collection point and go towards it and get aligned for pick-up. Show that robot knows that it has arrived at collection point.
