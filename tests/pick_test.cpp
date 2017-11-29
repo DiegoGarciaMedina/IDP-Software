5aad6af76e28a08919ebd7d875ab2d991890ffba
L.M.H. Gansser-Potts <lmhg2@ew128.eng.cam.ac.uk>
2017-11-29 12:06:14 +0000

    this is the test file that ive been using to just run any test

---
+++ tests/pick_test.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <stopwatch.h>
+#include <vector>
+#include <robot_instr.h>
+#include <robot_link.h>
+using namespace std;
+#define ROBOT_NUM 14
+robot_link rlink;
+#include "movement.h"
+#include "decision.h"
+int val; 
+
+int main(){
+stopwatch watch;
+
+#ifdef __arm__
+   if (!rlink.initialise ("127.0.0.1")) {       // setup for local hardware
+#else
+   if (!rlink.initialise (ROBOT_NUM)) {
+	   // setup the link
+#endif
+}
+
+  val = rlink.request (TEST_INSTRUCTION); // send test instruction
+  if (val == TEST_INSTRUCTION_RESULT) {   // check result
+    cout << "Test passed" << endl;
+  }
+  else if (val == REQUEST_ERROR) {
+    cout << "Fatal errors on link:" << endl;
+    rlink.print_errs();
+    rlink.reinitialise(); // flush the link
+  }
+  else {
+    cout << "Test failed (bad value returned)" << endl;
+
+}
+/*watch.start();
+while (watch.read() <500000){object_ahead();}
+
+
+int sensors;
+watch.start();
+while (watch.read()<3000)
+{sensors = sensors_read();
+line_following(sensors);}
+stop();*/
+
+alignment_pickup();
+
+return 0;}
